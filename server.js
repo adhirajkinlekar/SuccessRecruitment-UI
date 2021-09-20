a452aa64ab282f5e491647a28c26d58274e597cd
Adhiraj Kinlekar <adhirajkinlekar123@gmail.com>
2021-09-21 01:03:02 +0530

    sever file changes

--- server.js
+++ server.js
@@ -1,8 +1,8 @@
 const express = require('express');
 const path = require('path');
 const ngApp = express();
-ngApp.use(express.static('./dist/success-recruitment'));
+ngApp.use(express.static('./dist/SuccessRecruitment'));
 ngApp.get('/*', function (request, response) {
-    response.sendFile(path.join(__dirname, '/dist/success-recruitment/index.html'));
+    response.sendFile(path.join(__dirname, '/dist/SuccessRecruitment/index.html'));
 });
 ngApp.listen(process.env.PORT || 8080);
