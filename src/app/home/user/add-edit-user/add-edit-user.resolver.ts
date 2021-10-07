914976fca536879d02c4d119529fa6ec54d68641
Adhiraj Kinlekar <adhirajkinlekar123@gmail.com>
2021-10-07 13:00:08 +0530

    added user edit page

--- src/app/home/user/add-edit-user/add-edit-user.resolver.ts
+++ src/app/home/user/add-edit-user/add-edit-user.resolver.ts
@@ -2,7 +2,7 @@ import { Injectable } from '@angular/core';
 import { Resolve, ActivatedRouteSnapshot } from '@angular/router';
 import {Router} from '@angular/router';
 import { catchError } from 'rxjs/operators';
-import { of } from 'rxjs';
+import { forkJoin, of } from 'rxjs';
 import { UserService } from '../user.service';
 
 @Injectable({
@@ -15,11 +15,21 @@ export class AddEditUserResolver implements Resolve<any> {
 constructor(private service: UserService,private router: Router) {}
 
 resolve(route: ActivatedRouteSnapshot) {
-   return this.service.getRoles().pipe(
-    catchError((error) => {
-      const message = `Retrieval error: ${error}`;
-      return of({ roles: null, error: message });
-    }))
-}
-  
+  console.log(route)
+  const userId = route.params['id'];
+       return forkJoin([
+        this.service.getRoles().pipe(
+          catchError((error) => {
+            const message = `Retrieval error: ${error}`;
+            return of({ roles: null, error: message });
+          }),
+        ),
+        route.queryParams.isEdit == 'true'?  this.service.getUser(userId).pipe(
+          catchError((error) => {
+            const message = `Retrieval error: ${error}`;
+            return of({ formValues: null, error: message });
+          }),
+        ) : of(null)
+      ])
+  }
 }
